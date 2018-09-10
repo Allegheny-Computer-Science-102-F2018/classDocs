76f6051a3d19018c3e7606ffb0779acd7b38b93d
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2018-09-10 14:19:41 -0400

    Update

---
+++ lessons/03_week_roots_python/sandbox/cars_ii.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Date: 10 Sept 2019
+# To run this program: python3 cars_ii.py
+# Version : i
+
+def redFunction():
+    print("\n *redFunction() called")
+    # called if "red" was entered at main()
+    print("  User entered Red, the car was a Subaru!")
+#end of redFunction()
+
+def blueFunction():
+    print("\n *blueFunction() called")
+    # called if "blue" was entered at main()
+    print("  User entered Blue, the car was a Toyota!")
+# end of blueFunction()
+
+
+def main(): # main function (driver)
+    # ask the user what color car they saw
+    #color = input("  What color car did you see? (options: blue or red) : ")
+    msg = "  What color car did you see? (options: blue or red) : "
+    color = str(input(msg))
+    print("  The entered color is <<", color,">> and the type is", type(color))
+    if color.upper() == 'BLUE': #helps with input: blue = BlUe = BLUE
+        blueFunction() # call this function if "blue"
+    elif color.upper() == 'RED':
+        redFunction() #call this function is "red"
+    else:
+        print("  I do not know that car! :-(")
+#end of main()
+
+######  Begin the program by calling the function  #######
+main() # start the program by calling the main() function. 
+
